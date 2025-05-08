3bc88a2993a0e8bdd4ae6c8c3dfc8dd9339a9abc
JonahThurston <jonah.m.thurston@gmail.com>
2025-05-08 16:36:04 -0600

    Add Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "jonahportfolio-7272c", appId: "1:483422842706:web:3ea99152cb0beeaaa3d48b", storageBucket: "jonahportfolio-7272c.firebasestorage.app", apiKey: "AIzaSyAz3T3PNYsfEVQ2NDZRxeu-uHIJILGx6xI", authDomain: "jonahportfolio-7272c.firebaseapp.com", messagingSenderId: "483422842706", measurementId: "G-DLM4QVQKW5" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
